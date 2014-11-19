472df93dff72f10c63d1d3995c29ebcf5d054e73
eivindveg <eivindveg@hotmail.com>
2014-11-19 17:50:45 +0100

    Make startable

--- src/main/java/com/kjelland/JavaFXplorerApplication.java
+++ src/main/java/com/kjelland/JavaFXplorerApplication.java
@@ -1,15 +1,26 @@
 package com.kjelland;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class JavaFXplorerApplication extends Application {
 
     public static void main(String[] args) {
         Application.launch(args);
+
     }
     @Override
     public void start(final Stage primaryStage) throws Exception {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(classLoader.getResource("window.fxml"));
+        Parent root = loader.load();
 
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
